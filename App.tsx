cde02d6e0b1c58f49cb9e5002942ba0d1bf1c9ac
Oscare101 <kirillfedortsev2000@gmail.com>
2024-03-14 12:46:18 +0200

    app icons

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {Dimensions, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import StatImage from './components/icons/StatImage';
 import CupsImage from './components/icons/CupsImage';
@@ -6,9 +6,14 @@ import TeamImage from './components/icons/TeamImage';
 import GunImage from './components/icons/GunImage';
 import NadeImage from './components/icons/NadeImage';
 
+const {width, height} = Dimensions.get('screen');
+
 export default function App() {
   return (
     <View>
+      <Text>
+        {width} * {height}
+      </Text>
       <StatImage stat="spray" theme="light" size={50} />
       <CupsImage cup="Crown" size={400} />
       <TeamImage team="Quazars" size={50} />
