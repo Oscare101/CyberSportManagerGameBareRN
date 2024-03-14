d72a7a445b20c3fbe5c9e2405596e7f414032764
Oscare101 <kirillfedortsev2000@gmail.com>
2024-03-14 11:37:13 +0200

    clean extra cup icons

--- App.tsx
+++ App.tsx
@@ -7,7 +7,7 @@ export default function App() {
   return (
     <View>
       <StatImage stat="spray" theme="light" size={50} />
-      <CupsImage cup="GrandSlam" size={400} />
+      <CupsImage cup="Prestige3" size={400} />
     </View>
   );
 }
