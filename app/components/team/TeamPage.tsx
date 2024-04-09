70a8de7f2096eec0391ff5538374a51e9e499590
Oscare101 <kirillfedortsev2000@gmail.com>
2024-04-09 12:40:05 +0300

    team trophies block

--- app/components/team/TeamPage.tsx
+++ app/components/team/TeamPage.tsx
@@ -1,10 +1,91 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {
+  Dimensions,
+  FlatList,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  useColorScheme,
+} from 'react-native';
 import React, {memo} from 'react';
+import {RootState} from '../../redux';
+import {useSelector} from 'react-redux';
+import {Team, Trophy} from '../../constants/interfaces/playerTeamInterfaces';
+import CupsImage from '../icons/CupsImage';
+import globalStyles from '../../constants/globalStyles';
+import {Tournament} from '../../constants/interfaces/tournamentInterfaces';
+import {useNavigation} from '@react-navigation/native';
+import text from '../../constants/text';
+import colors from '../../constants/colors';
+
+const width = Dimensions.get('screen').width;
 
 function TeamPage() {
+  const systemTheme = useColorScheme();
+  const theme = useSelector((state: RootState) => state.theme);
+  const themeColor: any = theme === 'system' ? systemTheme : theme;
+  const navigation: any = useNavigation();
+
+  const teams: Team[] = useSelector((state: RootState) => state.teams);
+  const tournaments: Tournament[] = useSelector(
+    (state: RootState) => state.tournaments,
+  );
+
+  const myTeam: Team = teams.find((t: Team) => t.yourTeam) as Team;
+
+  function RenderTrophyItem({item}: any) {
+    return (
+      <TouchableOpacity
+        style={[globalStyles.columnCenter]}
+        activeOpacity={0.8}
+        onPress={() => {
+          navigation.navigate('TournamentScreen', {
+            tournament: tournaments.find(
+              (t: Tournament) =>
+                t.name === item.tournament && t.season === item.season,
+            ),
+          });
+        }}>
+        <CupsImage
+          cup={
+            tournaments.find(
+              (t: Tournament) =>
+                t.name === item.tournament && t.season === item.season,
+            )!.cup
+          }
+          size={width * 0.15}
+        />
+        <Text
+          style={{
+            fontSize: width * 0.04,
+            color: colors[themeColor].main,
+            fontWeight: '300',
+          }}>
+          {text.Season} {item.season}
+        </Text>
+      </TouchableOpacity>
+    );
+  }
+
   return (
-    <View>
-      <Text>TeamPage</Text>
+    <View
+      style={[
+        globalStyles.rowBetween,
+        {
+          backgroundColor: colors[themeColor].card,
+          width: '92%',
+          alignSelf: 'center',
+          borderRadius: width * 0.02,
+          padding: width * 0.02,
+          marginTop: width * 0.05,
+        },
+      ]}>
+      <FlatList
+        horizontal
+        data={myTeam.trophies}
+        renderItem={RenderTrophyItem}
+        ItemSeparatorComponent={() => <View style={{width: width * 0.03}} />}
+      />
     </View>
   );
 }
